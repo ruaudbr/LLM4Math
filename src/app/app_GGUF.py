8858cffa4a25651f8fbade7a4594737af030e7ff
accoumar12 <nicolas.pllr1@gmail.com>
2023-12-14 19:42:34 +0100

    code formatting

--- src/app/app_GGUF.py
+++ src/app/app_GGUF.py
@@ -1,9 +1,8 @@
 import gradio as gr
-
 from ctransformers import AutoModelForCausalLM
-
 import sys
 import os
+
 
 def load_available_models_paths(path):
     models_paths = {}
@@ -14,11 +13,12 @@ def load_available_models_paths(path):
                 models_paths[file] = file_path
     return models_paths
 
+
 def load_model(model_name, gpu_layer):
     MODEL_PATH = available_models_paths[model_name]
-    
-    global llm_is_loaded, llm
-    
+
+    global llm, llm_is_loaded
+
     if "llama" in MODEL_PATH:
         model_type = "llama2"
     else:
@@ -29,40 +29,43 @@ def load_model(model_name, gpu_layer):
         llm = AutoModelForCausalLM.from_pretrained(
             MODEL_PATH,
             model_type=model_type,
-            gpu_layers=gpu_layer
+            gpu_layers=gpu_layer,
         )
         print("Model loaded successfully :)")
         llm_is_loaded = True
         return llm
-    
+
     except Exception as e:
         print(f"Error loading model : {e}")
         return None
-    
-def predict(message, history):
 
+
+def predict(message, history):
     if llm is None or not llm_is_loaded:
         return "No LLM has been loaded yet ..."
-    
+
     dialogue_history_to_format = history + [[message, ""]]
-    messages = "".join(["".join(["\n<human>:"+item[0], "\n<bot>:"+item[1]])
-                   for item in dialogue_history_to_format])
-    
-    print("Started generating text ...")    
+    messages = "".join(
+        [
+            "".join(["\n<human>:" + item[0], "\n<bot>:" + item[1]])
+            for item in dialogue_history_to_format
+        ]
+    )
+
+    print("Started generating text ...")
     partial_message = ""
     for new_token in llm(messages, stream=True):
         if new_token != "<":
             partial_message += new_token
             yield partial_message
     print("Done generating text :)")
-        
+
 
 def gradio_app(models_path):
-
     # Load the model and its tokenizer
     global available_models_paths
     available_models_paths = load_available_models_paths(models_path)
-    
+
     # Initialize the llm. Will be chosen by the user
     global llm_is_loaded, llm
     llm, llm_is_loaded = None, False
@@ -70,21 +73,30 @@ def gradio_app(models_path):
     # Create a Gradio Chatbat Interface
     with gr.Blocks() as iface:
         with gr.Tab("Teacher Assistant"):
-            gr.ChatInterface(predict)           
-            
+            gr.ChatInterface(predict)
+
         with gr.Tab("Model choice and Options"):
             model_name_chosen = gr.Dropdown(available_models_paths.keys())
-            gpu_layers_chosen = gr.Slider(0, 5000, step=1, info="#layers to off-load on GPU")
+            gpu_layers_chosen = gr.Slider(
+                0,
+                5000,
+                step=1,
+                info="#layers to off-load on GPU",
+            )
             b1 = gr.Button("Load model")
-            b1.click(load_model, inputs=[model_name_chosen, gpu_layers_chosen], outputs=llm)
-    
+            b1.click(
+                load_model,
+                inputs=[model_name_chosen, gpu_layers_chosen],
+                outputs=llm,
+            )
+
     # Launch Gradio Interface
     print("Launching Gradio Interface...")
     iface.launch()
 
 
 ### How to use this script : ###
-# python src/app/app_GGUF.py 
+# python src/app/app_GGUF.py
 # python src/app/app_GGUF.py "llama2" "llama-2-7b-chat.Q4_K_M.gguf"
 if __name__ == "__main__":
     if len(sys.argv) >= 3:
