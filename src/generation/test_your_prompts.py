3ad03c70a4adcd952245d40e84a0f0d679bd82d5
accoumar12 <nicolas.pllr1@gmail.com>
2023-12-15 18:16:36 +0100

    I/O change: reads a .csv, outputs a .csv

--- src/generation/test_your_prompts.py
+++ src/generation/test_your_prompts.py
@@ -1,20 +1,20 @@
 import sys
-import json
 from tqdm import tqdm
 
-from constants import MODELS_ID, PROMPTS
+import pandas as pd
+
+from constants import MODELS_ID
 from utils import load_model
 
 # Parsing aguments
 arg = sys.argv
 if len(arg) != 4:
     raise Exception(
-        f"Invalid syntax calling this script. Please use the following :\n python {arg[0]} model_name precision input_file"
+        f"Invalid syntax calling this script. Please use the following :\n python {arg[0]} model_name precision prompt_file_name"
     )
 model_name, precision = arg[1], arg[2]
 
-###### Model name ######
-
+###### Model name #####
 if model_name not in MODELS_ID:
     model_names = "\n".join(MODELS_ID.keys())
     raise Exception(f"Invalide model name :(\nValid model names are : \n{model_names}")
@@ -24,15 +24,26 @@ model_id = MODELS_ID[model_name]
 if precision not in ["4", "8", "16", "32"]:
     raise Exception("Invalide model name :(\nValid precisions are : 4, 8, 16 or 32")
 
-
 ###### Loading the model ######
 print(f"Loading model : {model_id}")
 tokenizer, model = load_model(model_id, precision)
 
 
+###### Fetching the prompts ######
+PROMPTS_FOLDER = "prompts/"
+file_name = arg[3]
+try:
+    prompts_df = pd.read_csv(PROMPTS_FOLDER + file_name)
+except FileNotFoundError:
+    raise Exception(
+        f"The prompts file {file_name} was not found in the folder {PROMPTS_FOLDER} :("
+    )
+prompt_col_name = prompts_df.columns[0]
+prompts = prompts_df[prompt_col_name].to_list()
+
 ###### Generating ######
-answers = {}
-for prompt_idx, prompt in tqdm(PROMPTS.items(), desc="Prompt"):
+answers = []
+for prompt_idx, prompt in tqdm(prompts, desc="Prompt"):
     prompt = prompt.strip()
     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 
@@ -48,11 +59,12 @@ for prompt_idx, prompt in tqdm(PROMPTS.items(), desc="Prompt"):
         max_length=1024,
     )
     answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    answers[prompt] = answer
+    answers.append(answer)
 
 
-###### Saving prompts:answers into a .json ######
-output_filename = f"answers_{model_name}_ {precision}.json"
-output_path = f"outputs/{output_filename}"
-with open(output_path, "w", encoding="utf-8") as output_file:
-    json.dump(answers, output_file, ensure_ascii=False, indent=4)
+###### Saving prompts:answers into a .csv ######
+assert len(prompts) == len(answers), "The number of prompts and answers is not the same"
+prompts_df["answer"] = answers
+ANSWERS_FOLDER = "generated_answers/"
+answers_file_name = f"answers_{model_name}_{precision}.csv"
+prompts_df.to_csv(ANSWERS_FOLDER + answers_file_name, index=False)
